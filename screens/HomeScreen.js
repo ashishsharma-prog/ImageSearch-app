3b3e8b1a375af831adc656891169ee96f7942cb3
Ashish <sharmaashishlfps@gmail.com>
2022-07-01 01:14:44 +0530

    Added home screen functionality

--- screens/HomeScreen.js
+++ screens/HomeScreen.js
@@ -1,14 +1,70 @@
-import { StyleSheet, Text, View } from 'react-native'
-import React from 'react'
+import React,{useState} from 'react'
+import { StyleSheet, Text, View,TextInput,Button,Image } from 'react-native'
 
-const HomeScreen = () => {
-  return (
-    <View>
-      <Text>HomeScreen</Text>
-    </View>
-  )
+
+const HomeScreen = ({navigation,route}) => {
+    const [textSearch,setTextSearch]=useState('')//to store the searching text
+    return (
+        <View style={styles.screen}>
+            <Text style={styles.text}> Welcome to the Photo Searcher App</Text>
+            <Image style={styles.logo} source={{uri:"https://vignette1.wikia.nocookie.net/battlefordreamislandfanfiction/images/7/7a/Indigo_Triangle_%282%29.png/revision/latest?cb=20150823183021"}}/>
+            <TextInput  style={{
+                    ...styles.Search,
+                    backgroundColor: 'lightgrey',
+                    color: 'black',
+                    borderRadius: 10
+                }}
+                value={textSearch}
+                onChangeText={(text) => setTextSearch(text)}
+                placeholder='Search for news'
+                placeholderTextColor='grey'
+                
+               
+                />
+                <View style={styles.btn}>
+                <Button 
+                color="#d26374"
+                 title="Search Here"
+                onPress={()=>{
+                    //when we press search button then this button navigate to the image screen with the params
+                    // of search text which while used to search the image in the api
+                    navigation.navigate('ImageScreen',{
+                        searchItem:textSearch
+                    })
+                }}
+                />
+                </View>
+                
+        </View>
+    )
 }
 
 export default HomeScreen
 
-const styles = StyleSheet.create({})
+const styles = StyleSheet.create({
+   
+    Search: {
+        paddingVertical: 10,
+        paddingHorizontal: 20,
+        borderRadius: 10,
+        fontSize: 15,
+    },
+    btn:{
+        padding: 20
+    },
+    
+    text:{
+        fontSize:18,
+        marginHorizontal:10,
+        color:"#d26374",
+        fontWeight:'bold',
+        marginVertical:20
+    },
+    logo:{
+        width:200,
+        height:200,
+        marginHorizontal:70,
+        padding: 20,
+        marginVertical:20
+    }
+})
--- screens/ImageScreen.js
+++ screens/ImageScreen.js
@@ -1,13 +1,20 @@
 import React,{useEffect,useState} from 'react'
 import { StyleSheet, Text, View,FlatList ,Image,TextInput,ActivityIndicator} from 'react-native'
 
-const ImageScreen = () => {
+const ImageScreen = ({navigation,route}) => {
+    console.log(route)
+
+    const SearchingItem = route.params?.searchItem
     const [Image,setImage]=useState([])
     const [isloading, setLoading] = useState(false);
 // here we fetch the image data
+// const ImageUrl=`https://api.flickr.com/services/rest?method=flickr.photos.search&api_key=6837cf241cfe54121c851291cc705fa7&format=json&text=${SearchingItem}&nojsoncallback=true&page=${currentPage}&perpage=100&extras=url_s`
+useEffect(()=>{
+    fetchImage()
+},[SearchingItem])
     const fetchImage = ()=>{
         setLoading(true);
-     const ImageUrl=`https://api.flickr.com/services/rest?method=flickr.photos.search&api_key=6837cf241cfe54121c851291cc705fa7&format=json&text=${SearchingItem}&nojsoncallback=true&page=${currentPage}&perpage=100&extras=url_s`
+     const ImageUrl=`https://api.flickr.com/services/rest?method=flickr.photos.search&api_key=6837cf241cfe54121c851291cc705fa7&format=json&text=${SearchingItem}&nojsoncallback=true&page=10&perpage=100&extras=url_s`
      console.log(ImageUrl)
      fetch(ImageUrl)
     .then((response)=>response.json())
